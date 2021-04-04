02d8b7cb3e3d1626b6b156e3fb57b9cd88a6d68a
Gapur Mogushkov <gapur@mail.ru>
2021-04-05 00:42:40 +0300

    overwrite

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,11 @@
 export default function orderByProps(objectSort, arrSort = []) {
-  let arrAllKey = [];
+  const arrAllKey = [];
   for (const key in objectSort) {
     if (arrSort.indexOf(key) === -1) {
       arrAllKey.push(key);
     }
   }
   arrAllKey = [...arrSort, ...arrAllKey.sort()];
-
   const result = [];
   for (const key of arrAllKey) {
     result.push({ key, value: objectSort[key] });
